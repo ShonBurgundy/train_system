8758ae8118f93f9099cecb6cbb0adfe1ceb8583c
shonburgundy <seanbeloussow@yahoo.com>
2020-05-20 13:26:57 -0700

    pass save method test in train.rb

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -9,15 +9,15 @@ describe '#Train' do
     end  
   end
 
-  # describe('#save') do
-  #   it('saves a train') do
-  #     train1 = Train.new({:name => "A", :id => nil})
-  #     train1.save
-  #     train2 = Train.new({:name => "B", :id => nil})
-  #     train2.save
-  #     expect(Train.all).to(eq([train1, train2]))
-  #   end
-  # end
+  describe('#save') do
+    it('saves a train') do
+      train1 = Train.new({:name => "A", :id => nil})
+      train1.save
+      train2 = Train.new({:name => "B", :id => nil})
+      train2.save
+      expect(Train.all).to(eq([train1, train2]))
+    end
+  end
 
   describe('#==') do
     it("is the same train if it has the same name") do
