e959e3e080bb34ccb14386e61fddce4498604771
shonburgundy <seanbeloussow@yahoo.com>
2020-05-20 14:28:44 -0700

    add fail tests for all, save & == methods in city.rb

--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -12,10 +12,20 @@ describe('#City') do
     it('saves a city') do
       city1 = City.new({:name => "Portland", id => nil})
       city1.save
-      city2 = City.new({:name => "Portland", id => nil})
-      city1.save
+      city2 = City.new({:name => "Seattle", id => nil})
+      city2.save
+      expect(Train.all).to(eq([city1, city2]))
     end
   end
 
+  describe('#==') do
+    it("is the same city if it has the same name") do
+      city1 = City.new({:name => "Portland", id => nil})
+      city2 = City.new({:name => "Portland", id => nil})
+      expect(city1).to(eq(city2))
+    end
+  end
+
+  
 
 end
